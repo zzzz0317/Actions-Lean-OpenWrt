e7c8b00a71d508e87dbf89c81a6b67780db589b9
zzzz0317 <zhangzhe0177@gmail.com>
2023-04-05 23:23:23 +0800

    202304052323

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,23 +11,23 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 取消Lean大登陆密码
-sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
 # 拉取PassWall源码
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/packages
+#git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/packages
 #git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/packages
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/luci
+#git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/luci
 #git clone -b luci-reality https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
 
 # 拉取luci-app-smartdns插件
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/lienol/luci-app-smartdns
 
 # 删除原版luci-app-softethervpn插件
-rm -rf feeds/luci/applications/luci-app-softethervpn
+#rm -rf feeds/luci/applications/luci-app-softethervpn
 
 # 拉取luci-app-softethervpn插件
-svn co https://github.com/lxhao61/openwrt-plugin/trunk/package/lean/luci-app-softethervpn feeds/luci/applications/luci-app-softethervpn
+#svn co https://github.com/lxhao61/openwrt-plugin/trunk/package/lean/luci-app-softethervpn feeds/luci/applications/luci-app-softethervpn
