e110bb75ac7bdfbbe4c28507d8e0d14ec28a92b5
zzzz0317 <zhangzhe0177@gmail.com>
2023-04-06 21:06:42 +0800

    202304062106

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,10 +20,10 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # 取消helloworld源注释（ssr-plus）
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 添加xiaorouji大passwall源
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 
 # 注释掉lean大默认packages源
 #sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
